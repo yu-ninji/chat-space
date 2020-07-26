977918c2d3f2b04b765e4636439873c2e53c980d
yu-ninji <ninjikamikami@icloud.com>
2020-07-26 22:02:12 +0900

    4

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable,  :validatable
+
+  has_many :group_users
+  has_many :groups, through: :group_users
+  has_many :messages
 
   validates :name, presence: true, uniqueness: true
-
 end
